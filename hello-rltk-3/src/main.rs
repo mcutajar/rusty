3fb67cfdc891746f9ca4dd355152ef6965f46efe
Matthew Cutajar <matthewcutajar83@gmail.com>
2025-06-06 00:51:05 +0200

    feat: add LeftMover struct & LeftWalker struct + system

--- hello-rltk-3/src/main.rs
+++ hello-rltk-3/src/main.rs
@@ -15,11 +15,19 @@ struct Renderable {
     bg: RGB,
 }
 
+#[derive(Component)]
+struct LeftMover {}
+
 struct State {
-    ecs: World
+    ecs: World,
 }
+
 impl GameState for State {
     fn tick(&mut self, ctx : &mut Rltk) {
+        ctx.cls();
+
+        self.run_systems();
+
         let positions = self.ecs.read_storage::<Position>();
         let renderables = self.ecs.read_storage::<Renderable>();
 
@@ -29,15 +37,40 @@ impl GameState for State {
     }
 }
 
+struct LeftWalker {}
+
+impl<'a> System<'a> for LeftWalker {
+    type SystemData = (ReadStorage<'a, LeftMover>,
+                       WriteStorage<'a, Position>);
+
+    fn run(&mut self, (lefty, mut pos) : Self::SystemData) {
+        for (_lefty,pos) in (&lefty, &mut pos).join() {
+            pos.x -= 1;
+            if pos.x < 0 { pos.x = 79; }
+        }
+    }
+}
+
+impl State {
+    fn run_systems(&mut self) {
+        let mut lw = LeftWalker{};
+        lw.run_now(&self.ecs);
+        self.ecs.maintain();
+    }
+}
+
 fn main() -> rltk::BError {
     use rltk::RltkBuilder;
     let context = RltkBuilder::simple80x50()
         .with_title("Roguelike Tutorial")
         .build()?;
-    
-    let mut gs = State{ ecs: World::new() };
+    let mut gs = State {
+        ecs: World::new()
+    };
     gs.ecs.register::<Position>();
     gs.ecs.register::<Renderable>();
+    gs.ecs.register::<LeftMover>();
+
     gs.ecs
         .create_entity()
         .with(Position { x: 40, y: 25 })
@@ -48,17 +81,18 @@ fn main() -> rltk::BError {
         })
         .build();
 
-    for i in 0..10 {
+    for i in 0..11 {
         gs.ecs
             .create_entity()
-            .with(Position { x: i * 7, y: 20 })
+            .with(Position { x: i * 8, y: 10 })
             .with(Renderable {
                 glyph: rltk::to_cp437('☺'),
                 fg: RGB::named(rltk::RED),
                 bg: RGB::named(rltk::BLACK),
             })
+            .with(LeftMover{})
             .build();
     }
-    
+
     rltk::main_loop(context, gs)
 }
