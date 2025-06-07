850bcd6f395821153d75f67820f6556af712a808
Matthew Cutajar <matthewcutajar83@gmail.com>
2025-06-07 09:50:22 +0200

    feat: add corridors to the randomly generated rooms

--- hello-rltk-10/src/map.rs
+++ hello-rltk-10/src/map.rs
@@ -73,6 +73,19 @@ pub fn new_map_rooms_and_corridors() -> Vec<TileType> {
         }
         if ok {
             apply_room_to_map(&new_room, &mut map);
+
+            if !rooms.is_empty() {
+                let (new_x, new_y) = new_room.center();
+                let (prev_x, prev_y) = rooms[rooms.len()-1].center();
+                if rng.range(0,2) == 1 {
+                    apply_horizontal_tunnel(&mut map, prev_x, new_x, prev_y);
+                    apply_vertical_tunnel(&mut map, prev_y, new_y, new_x);
+                } else {
+                    apply_vertical_tunnel(&mut map, prev_y, new_y, prev_x);
+                    apply_horizontal_tunnel(&mut map, prev_x, new_x, new_y);
+                }
+            }
+
             rooms.push(new_room);
         }
     }
@@ -115,8 +128,17 @@ pub fn draw_map(map: &[TileType], ctx: &mut Rltk) {
     }
 }
 
-fn _apply_horizontal_tunnel(map: &mut [TileType], x1:i32, x2:i32, y:i32) {
+fn apply_horizontal_tunnel(map: &mut [TileType], x1:i32, x2:i32, y:i32) {
     for x in min(x1,x2) ..= max(x1,x2) {
+        let idx = xy_idx(x, y);
+        if idx > 0 && idx < 80*50 {
+            map[idx as usize] = TileType::Floor;
+        }
+    }
+}
+
+fn apply_vertical_tunnel(map: &mut [TileType], y1:i32, y2:i32, x:i32) {
+    for y in min(y1,y2) ..= max(y1,y2) {
         let idx = xy_idx(x, y);
         if idx > 0 && idx < 80*50 {
             map[idx as usize] = TileType::Floor;
