03c8c38f222a830847420ec76d97bae92d04a982
Matthew Cutajar <matthewcutajar83@gmail.com>
2025-06-07 10:25:58 +0200

    feat: add support for more keys for movement and added diagonal movement

--- hello-rltk-10/src/player.rs
+++ hello-rltk-10/src/player.rs
@@ -22,10 +22,38 @@ pub fn player_input(gs: &mut State, ctx: &mut Rltk) {
     match ctx.key {
         None => {} // Nothing happened
         Some(key) => match key {
-            VirtualKeyCode::Left => try_move_player(-1, 0, &mut gs.ecs),
-            VirtualKeyCode::Right => try_move_player(1, 0, &mut gs.ecs),
-            VirtualKeyCode::Up => try_move_player(0, -1, &mut gs.ecs),
-            VirtualKeyCode::Down => try_move_player(0, 1, &mut gs.ecs),
+            // ←
+            VirtualKeyCode::Left |
+            VirtualKeyCode::Numpad4 |
+            VirtualKeyCode::A |
+            VirtualKeyCode::H => try_move_player(-1, 0, &mut gs.ecs),
+
+            // →
+            VirtualKeyCode::Right |
+            VirtualKeyCode::Numpad6 |
+            VirtualKeyCode::D |
+            VirtualKeyCode::L => try_move_player(1, 0, &mut gs.ecs),
+
+            // ↑
+            VirtualKeyCode::Up |
+            VirtualKeyCode::Numpad8 |
+            VirtualKeyCode::W |
+            VirtualKeyCode::K => try_move_player(0, -1, &mut gs.ecs),
+
+            // ↓
+            VirtualKeyCode::Down |
+            VirtualKeyCode::Numpad2 |
+            VirtualKeyCode::S |
+            VirtualKeyCode::J => try_move_player(0, 1, &mut gs.ecs),
+            
+            // ↙
+            VirtualKeyCode::Numpad1 => try_move_player(-1, 1, &mut gs.ecs),
+            // ↖
+            VirtualKeyCode::Numpad7 => try_move_player(-1, -1, &mut gs.ecs),
+            // ↘
+            VirtualKeyCode::Numpad3 => try_move_player(1, 1, &mut gs.ecs),
+            // ↗
+            VirtualKeyCode::Numpad9 => try_move_player(1, -1, &mut gs.ecs),
             _ => {}
         },
     }
